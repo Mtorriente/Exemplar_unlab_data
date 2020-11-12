2074f67b8dce2df546d8a647a6a1eff2becc2b5b
Mtorriente <72882558+Mtorriente@users.noreply.github.com>
2020-11-12 18:01:03 +0100

    Update splitting_data_small_val.py

--- surrogate_dataset/splitting_data_small_val.py
+++ surrogate_dataset/splitting_data_small_val.py
@@ -14,7 +14,7 @@ except:
 val_size = 0.1                # percentage/100 of the full dataset for validation
 train_size = 1 - val_size     # only valid if there is no test set
 
-classes = os.listdir('./data/') # create list with all the class names (folders)
+classes = os.listdir('./datos_ampliados/') # create list with all the class names (folders)
 classes.sort()
 
 for class_i in tqdm(classes):
@@ -25,15 +25,15 @@ for class_i in tqdm(classes):
     except:
         print ('Class folder already existing... ???')
 
-    images = os.listdir('./data/' + class_i)
+    images = os.listdir('./data_ampliados/' + class_i)
     images.sort()
 
     for image in images:
         if np.random.uniform(0,1) >= train_size:
-            copyfile('./data/' + str(class_i).zfill(len(str(len(classes)))) + '/' + image,
+            copyfile('./data_ampliados/' + str(class_i).zfill(len(str(len(classes)))) + '/' + image,
                      './val_set/' + str(class_i).zfill(3) + '/' + image)
         else:
-            copyfile('./data/' + str(class_i).zfill(len(str(len(classes)))) + '/' + image,
+            copyfile('./data_ampliados/' + str(class_i).zfill(len(str(len(classes)))) + '/' + image,
                      './train_set/' + str(class_i).zfill(len(str(len(classes)))) + '/' + image)
         # change the previous else for an elif in
         # the case that you want a test split as well...
