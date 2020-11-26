76376cd6d544cc3c9c2c8a830ab2f25de4b1ea1d
Mtorriente <72882558+Mtorriente@users.noreply.github.com>
2020-11-26 17:37:06 +0100

    Add files via upload

---
+++ surrogate_dataset/compute_edge_masks.py
@@ -0,0 +1,24 @@
+import os
+import numpy as np
+from PIL import Image
+from skimage import color
+from skimage.filters import sobel
+from tqdm import tqdm
+
+# Converting the class images into "edge masks"
+out_path = './surrogate_dataset/edge_masks/'
+in_path = './surrogate_dataset/classes/'
+
+classes = os.listdir(in_path)
+classes.sort()
+
+try:
+    os.mkdir(out_path)
+except:
+    pass
+
+for img in tqdm(classes):
+    image = np.asarray(Image.open(in_path + img))
+    edge_mask = sobel(color.rgb2gray(image))
+    im = Image.fromarray(np.uint8(edge_mask*255))
+    im.save(out_path + img[:-4] + '_edge.png')
